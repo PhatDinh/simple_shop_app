d1307e8462309b2d2ed432b30e383ca30db0e63e
PhatDinh <dinhphat611@gmail.com>
2022-02-20 20:03:33 +0700

    Add listener

--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -10,21 +10,20 @@ class ProductGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productData = Provider.of<Products>(context);
-    final loadedProduct = productData.items;
+    final products = productData.items;
 
     return GridView.builder(
         padding: const EdgeInsets.all(10),
-        itemCount: loadedProduct.length,
+        itemCount: products.length,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
         ),
-        itemBuilder: (ctx, index) => ProductItem(
-            id: loadedProduct[index].id,
-            price: loadedProduct[index].price,
-            title: loadedProduct[index].title,
-            imgUrl: loadedProduct[index].imageUrl));
+        itemBuilder: (ctx, i) => ChangeNotifierProvider(
+              create: (c) => products[i],
+              child: ProductItem(),
+            ));
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
 import '../screens/product_detail_screen.dart';
 
 import '../models/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imgUrl;
-  final double price;
-
-  ProductItem({this.id, this.title, this.imgUrl, this.price});
-
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(15),
       child: GridTile(
         child: GestureDetector(
-          onTap: () => Navigator.pushNamed(context, '/detail', arguments: id),
+          onTap: () =>
+              Navigator.pushNamed(context, '/detail', arguments: product.id),
           child: Image.network(
-            imgUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
@@ -31,20 +29,22 @@ class ProductItem extends StatelessWidget {
             color: Theme.of(context).buttonColor,
           ),
           leading: IconButton(
-            onPressed: () {},
+            onPressed: () {
+              product.toggleFavorite();
+            },
             icon: Icon(
-              Icons.favorite,
+              product.isFavorite ? Icons.favorite : Icons.favorite_border,
             ),
             color: Theme.of(context).buttonColor,
           ),
           backgroundColor: Colors.black87,
           title: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
             Text(
-              title,
+              product.title,
               textAlign: TextAlign.center,
             ),
             Text(
-              price.toString(),
+              product.price.toString(),
               textAlign: TextAlign.center,
             )
           ]),
